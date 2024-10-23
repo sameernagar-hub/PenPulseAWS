d28578c548c53fb4b52d7d92695adeb8b20280d0
Donald Novasky <dnovasky@users.noreply.github.com>
2024-10-22 21:32:26 -0700

    fixed typo and removed unnecessary installs

--- terraform/penpulse.sh
+++ terraform/penpulse.sh
@@ -12,8 +12,6 @@ sudo systemctl start httpd.service
 # Enable Apache to start at boot
 sudo systemctl enable httpd.service
 
-type -p yum-config-manager >/dev/null || sudo yum install yum-utils
-sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo yum install git -y
-g clone https://github.com/sameernagar-hub/PenPulseAWS.git
+git clone https://github.com/sameernagar-hub/PenPulseAWS.git
 sudo mv PenPulseAWS/* /var/www/html/
